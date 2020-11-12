45196ff62116eb0527ab4cae7a946fd530308e22
KobeHill68 <kobehill68@gmail.com>
2020-11-11 18:03:16 -0600

    layed out the readme file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,53 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  [![GitHub license](https:/img.shields.io/badge/license-MIT-blue.svg)]
+
+
+  ## User Story
+
+  ${data.userStory}
+
+
+  ## Table of Contents
+
+  * [Installation](#installationInstructions)
+  * [Usage](#usageInformation)
+  * [Contribution Guidelines](#contributionGuidelines)
+  * [Testing Instructions](#testInstructions)
+  * [License](#license)
+  * [GitHub Username](#github)
+  * [Email Address](#email)
+  
+
+  ## Installation
+
+
+
+  ## Usage
+
+
+
+  ## Contribution Guidelines
+
+
+
+  ## Testing Instructions
+
+
+
+  ## License
+
+
+
+  ## GitHub Username
+
+
+
+  ## Email Address
+
+
+  
 `;
 }
 
